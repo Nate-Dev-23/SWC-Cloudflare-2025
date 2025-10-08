6d84787d04a0f3f770bb45b68544a0aaa6db4554
fashionaisoftware <fashionaisoftware@gmail.com>
2025-10-08 12:06:32 +0200

    updated success page for better visibility

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function Success() {
   return (
-    <div>
-        <h1>Cloudflare migration was successful, you can now buy table 58 basket with Keira</h1>
-        <h2>(and some freeze dried mallows!!</h2>
+    <div className='flex flex-col pt-16'>
+        <h1 className='mx-auto mt-24 text-4xl font-bold' >Cloudflare migration was successful, you can now buy table 58 basket</h1>
+        <h2 className='mx-auto mb-24'>and some freeze dried mallows!!</h2>
     </div>
   )
 }
