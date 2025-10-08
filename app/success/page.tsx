ddafaaad94f971335feace1c3d8cdbba4e9d93e8
fashionaisoftware <fashionaisoftware@gmail.com>
2025-10-08 12:02:45 +0200

    added succcess page with the purpose of testing migration to cloudflare from hetzner

---
+++ app/success/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Success() {
+  return (
+    <div>
+        <h1>Cloudflare migration was successful, you can now buy table 58 basket with Keira</h1>
+        <h2>(and some freeze dried mallows!!</h2>
+    </div>
+  )
+}
